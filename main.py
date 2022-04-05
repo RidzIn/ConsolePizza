57e9518d7303949d25af987fc2d58f1e865524f5
Danylo Dorofieiev <dorofieiev.dd@gmail.com>
2022-04-05 12:44:17 +0200

    This is welcome commit

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("Welcome to Ridz Pizza")
-
+"""
+This is my first console project written in Python. In there I will try to
+practice the basics of Python and using GIT.
+"""
+print("\t\t\tWelcome to my console application called: 'Ridz Pizza'")
