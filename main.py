a02eaa311f1fcefa1ba4795e340241e050624e9a
Danylo Dorofieiev <dorofieiev.dd@gmail.com>
2022-04-05 13:04:49 +0200

    This is commit at Main Branch

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ This is my first console project written in Python. In there I will try to
 practice the basics of Python and using GIT.
 """
 print("\t\t\tWelcome to my console application called: 'Ridz Pizza'")
+
+"""
+I am making commit at Main Branch
+"""
